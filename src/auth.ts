8ecae8417c4696bdb88277636e87ebe1c2d90495
Luis Sáenz <luis.saenz@tropipay.com>
2024-11-11 18:35:11 -0300

    basic config 2

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,25 @@
 import NextAuth from "next-auth"
+import Credentials from "next-auth/providers/credentials"
+// Your own logic for dealing with plaintext password strings; be careful!
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+  providers: [
+    Credentials({
+      credentials: {
+        email: {},
+        password: {},
+      },
+      authorize: async (credentials) => {
+        let user = null
+
+        user = await getUserFromDb(credentials.email, credentials.password)
+
+        if (!user) {
+          throw new Error("Invalid credentials.")
+        }
+
+        return user
+      },
+    }),
+  ],
 })
