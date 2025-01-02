1d88526af4919ff636b617d55359e29b602fca57
Arliz Heredia Rodríguez <arliz.heredia@tropipay.com>
2025-01-02 18:00:40 -0500

    feat: add i18n pt support

--- src/components/intl/utils.ts
+++ src/components/intl/utils.ts
@@ -1,28 +1,16 @@
 import Cookies from "js-cookie"
 import English from "./messages/en.json"
 import Spanish from "./messages/es.json"
+import Portuguese from "./messages/pt.json"
 import { Languages } from "./types"
 
 const LOCALE_STORAGE_VAR = "userLang"
 
-export const LANG_DEFAULT = "es"
+export const LANG_DEFAULT = "en"
 
-export const supportedLanguages: Languages[] = ["es", "en"]
+export const supportedLanguages: Languages[] = ["es", "en", "pt"]
 
-export const getLocaleFromBrowser = () => {
-  if (typeof navigator != "undefined" && navigator.language) {
-    if (navigator.language.includes("en")) {
-      return "en"
-    }
-    if (navigator.language.includes("es")) {
-      return "es"
-    }
-  }
-  return LANG_DEFAULT
-}
-
-export const getLocaleStored = () =>
-  Cookies.get(LOCALE_STORAGE_VAR) ?? getLocaleFromBrowser()
+export const getLocaleStored = () => Cookies.get(LOCALE_STORAGE_VAR)
 
 export const setLocaleStored = (locale: string) => {
   Cookies.set(LOCALE_STORAGE_VAR, locale, {
@@ -32,5 +20,15 @@ export const setLocaleStored = (locale: string) => {
   })
 }
 
-export const getLocaleI18nResource = (locale: string): Record<string, string> =>
-  locale === "en" ? English : Spanish
+export const getLocaleI18nResource = (
+  locale: string
+): Record<string, string> => {
+  switch (locale) {
+    case "es":
+      return Spanish
+    case "pt":
+      return Portuguese
+    default:
+      return English
+  }
+}
