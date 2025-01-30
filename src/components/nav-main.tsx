bcc5610c0cf0eb0cc56909c6cd84b31fd6146b70
Arliz Heredia Rodríguez <arliz.heredia@tropipay.com>
2025-01-30 02:15:20 -0500

    fix: show submenu options on collapsed menu

--- src/components/nav-main.tsx
+++ src/components/nav-main.tsx
@@ -8,6 +8,13 @@ import {
   CollapsibleTrigger,
 } from "@/components/ui/collapsible"
 import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import {
   SidebarGroup,
   SidebarMenu,
   SidebarMenuButton,
@@ -15,6 +22,7 @@ import {
   SidebarMenuSub,
   SidebarMenuSubButton,
   SidebarMenuSubItem,
+  useSidebar,
 } from "@/components/ui/sidebar"
 import { SidebarOption } from "@/types/sidebarOption"
 import React from "react"
@@ -23,6 +31,7 @@ import { useTranslation } from "./intl/useTranslation"
 
 export function NavMain({ items }: { items: SidebarOption[] }) {
   const { t } = useTranslation()
+  const { isMobile } = useSidebar()
 
   return (
     <SidebarGroup>
@@ -37,11 +46,42 @@ export function NavMain({ items }: { items: SidebarOption[] }) {
               >
                 <SidebarMenuItem>
                   <CollapsibleTrigger asChild>
-                    <SidebarMenuButton tooltip={t(item.title)}>
-                      {item.icon && <item.icon />}
-                      <FormattedMessage id={item.title} />
-                      <ChevronRight className="ml-auto transition-transform duration-200 group-data-[state=open]/collapsible:rotate-90" />
-                    </SidebarMenuButton>
+                    <>
+                      <SidebarMenuButton
+                        tooltip={t(item.title)}
+                        className="group-data-[collapsible=icon]:hidden"
+                      >
+                        {item.icon && <item.icon />}
+                        <FormattedMessage id={item.title} />
+                        <ChevronRight className="ml-auto transition-transform duration-200 group-data-[state=open]/collapsible:rotate-90" />
+                      </SidebarMenuButton>
+
+                      <div className="hidden group-data-[collapsible=icon]:block">
+                        <DropdownMenu>
+                          <DropdownMenuTrigger>
+                            <SidebarMenuButton tooltip={t(item.title)}>
+                              {item.icon && <item.icon />}
+                            </SidebarMenuButton>
+                          </DropdownMenuTrigger>
+                          <DropdownMenuContent
+                            className="w-[--radix-dropdown-menu-trigger-width] min-w-56 rounded-lg"
+                            side={isMobile ? "bottom" : "right"}
+                            sideOffset={4}
+                            align={"start"}
+                          >
+                            <DropdownMenuGroup>
+                              {item.items?.map((subItem) => (
+                                <DropdownMenuItem>
+                                  <a href={subItem.url}>
+                                    <FormattedMessage id={subItem.title} />
+                                  </a>
+                                </DropdownMenuItem>
+                              ))}
+                            </DropdownMenuGroup>
+                          </DropdownMenuContent>
+                        </DropdownMenu>
+                      </div>
+                    </>
                   </CollapsibleTrigger>
                   <CollapsibleContent>
                     <SidebarMenuSub>
