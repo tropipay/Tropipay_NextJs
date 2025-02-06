a88f2cb288bdd50e4529d89b24d59b0afa316b56
Luis Sáenz <luis.saenz@tropipay.com>
2025-02-06 09:02:53 -0300

    split filters

---
+++ src/app/queryDefinitions/movements/filters.ts
@@ -0,0 +1,48 @@
+import {
+  movementsState,
+  movementTypes,
+  paymentMethods,
+} from "@/app/filterDefinitions/definitions"
+
+export const movementsFilters = [
+  {
+    type: "amount",
+    column: "amount",
+    label: "amount",
+  },
+  {
+    type: "list",
+    column: "status",
+    label: "status",
+    options: movementsState,
+  },
+  {
+    type: "date",
+    column: "valueDate",
+    label: "date",
+  },
+  {
+    type: "list",
+    column: "movementType",
+    label: "type",
+    options: movementTypes,
+  },
+  {
+    type: "list",
+    column: "paymentMethod",
+    label: "method",
+    options: paymentMethods,
+  },
+  {
+    type: "uniqueValue",
+    column: "sender",
+    label: "user",
+    placeHolder: "user",
+  },
+  {
+    type: "uniqueValue",
+    column: "reference",
+    label: "reference",
+    placeHolder: "reference",
+  },
+]
