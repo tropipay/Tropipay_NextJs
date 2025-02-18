08cf05b39f4fa41e59a0d2d38f80d85bf4c7e602
Arliz Heredia Rodríguez <arliz.heredia@tropipay.com>
2025-02-18 14:33:05 -0500

    fix: get token from login

--- src/lib/utilsUser.ts
+++ src/lib/utilsUser.ts
@@ -7,18 +7,11 @@ export const getUser = async () => {
   return await JSON.stringify(session)
 }
 
-export const decodeSession = (session?: string) => {
-  try {
-    return session
-      ? JSON.parse(decodeURIComponent(session || "")).sessionData
-      : {}
-  } catch (e) {
-    return {}
-  }
-}
+export const getTokenFromSession = (session?: any): string => {
+  const {
+    sessionData: { token },
+  } = session
 
-export const getTokenFromSession = (session?: string): string => {
-  const { token } = decodeSession(session)
   return token ?? ""
 }
 
