70df679bfc032ce492ee7a0f01b801764d01d166
Arliz Heredia Rodríguez <arliz.heredia@tropipay.com>
2025-04-02 21:40:14 -0400

    refactor: movement details UI

--- src/app/dashboard/charges/chargeDetail.tsx
+++ src/app/dashboard/charges/chargeDetail.tsx
@@ -33,7 +33,7 @@ export default function ChargeDetail(props: any): JSX.Element {
     country,
     cardExpirationDate,
     cardCountry,
-    cardIp,
+    clientIp,
   } = row
 
   const onDownloadInvoiceFile = async () => {
@@ -65,100 +65,98 @@ export default function ChargeDetail(props: any): JSX.Element {
   }
 
   return (
-    <>
-      <div className="max-w-md mx-auto p-4">
-        <div className="flex justify-between items-center mb-3">
-          <div className="font-poppins text-2xl leading-5 tracking-tight uppercase font-bold">
-            {amount.value > 0 ? "+" : ""}
-            {formatAmount(amount.value, amount.currency, "right")}
-          </div>
-          <FacetedBadge
-            value={state}
-            optionList={chargeStates}
-            optionListGroups={chargeStatesGroups}
-          />
+    <div className="max-w-md mx-auto p-4">
+      <div className="flex justify-between items-center mb-3">
+        <div className="font-poppins text-2xl leading-5 tracking-tight uppercase font-bold">
+          {amount.value > 0 ? "+" : ""}
+          {formatAmount(amount.value, amount.currency, "right")}
         </div>
-        <div className="flex justify-between items-center mb-4 pb-1">
-          <p className="text-xs text-gray-500">
-            <FormattedMessage id="charge_to" /> {fullName}
-          </p>
-          {completedAt && (
-            <p className="text-xs text-gray-500">
-              {format(new Date(completedAt), "dd/MM/yy HH:mm")}
-            </p>
-          )}
-        </div>
-        <Section title={<FormattedMessage id="payment_details" />}>
-          <Info
-            label={<FormattedMessage id="amount" />}
-            value={formatAmount(amount.value, amount.currency, "right")}
-          />
-          <Info
-            label={<FormattedMessage id="paymentMethod" />}
-            value={<FormattedMessage id={`pm_${paymentMethod}`} />}
-          />
-          <Info
-            label={<FormattedMessage id="cardBin" />}
-            value={`${cardBin} **** `}
-          />
-          <Info label={<FormattedMessage id="reference" />} value={reference} />
-          <Info label={<FormattedMessage id="errorCode" />} value={errorCode} />
-        </Section>
-
-        <Section title={<FormattedMessage id="client_data" />}>
-          <Info label={<FormattedMessage id="fullName" />} value={fullName} />
-          <Info label={<FormattedMessage id="email" />} value={email} />
-          <Info label={<FormattedMessage id="address" />} value={address} />
-          <Info label={<FormattedMessage id="country" />} value={country} />
-        </Section>
-
-        <Section title={<FormattedMessage id="payment_methods" />}>
-          <Info
-            label={<FormattedMessage id="cardPan" />}
-            value={`**** ${cardPan}`}
-          />
-          {cardExpirationDate && (
-            <Info
-              label={<FormattedMessage id="cardExpirationDate" />}
-              value={format(cardExpirationDate, "dd/MM/yy")}
-            />
-          )}
-          <Info
-            label={<FormattedMessage id="cardCountry" />}
-            value={cardCountry}
-          />
-          <Info label={<FormattedMessage id="cardIp" />} value={cardIp} />
-        </Section>
-
-        <Section title={<FormattedMessage id="schedule" />}>
-          {createdAt && (
-            <Info
-              label={<FormattedMessage id="createdAt" />}
-              value={createdAt && format(new Date(createdAt), "dd/MM/yy")}
-            />
-          )}
-          {completedAt && (
-            <Info
-              label={<FormattedMessage id="completedAt" />}
-              value={completedAt && format(new Date(completedAt), "dd/MM/yy")}
-            />
-          )}
-        </Section>
-
-        <div className="flex mt-4 gap-4">
-          <Button
-            variant="outline"
-            className="w-full md:w-1/2"
-            onClick={onDownload}
-          >
-            <Download />
-            <FormattedMessage id="download" />
-          </Button>
-          <Button variant="default" className="w-full md:w-1/2">
-            <FormattedMessage id="refound" />
-          </Button>
-        </div>
+        <FacetedBadge
+          value={state}
+          optionList={chargeStates}
+          optionListGroups={chargeStatesGroups}
+        />
       </div>
-    </>
+      <div className="flex justify-between items-center mb-4 pb-1">
+        <p className="text-xs text-gray-500">
+          <FormattedMessage id="charge_to" /> {fullName}
+        </p>
+        {completedAt && (
+          <p className="text-xs text-gray-500">
+            {format(new Date(completedAt), "dd/MM/yy HH:mm")}
+          </p>
+        )}
+      </div>
+      <Section title={<FormattedMessage id="payment_details" />}>
+        <Info
+          label={<FormattedMessage id="amount" />}
+          value={formatAmount(amount.value, amount.currency, "right")}
+        />
+        <Info
+          label={<FormattedMessage id="paymentMethod" />}
+          value={<FormattedMessage id={`pm_${paymentMethod}`} />}
+        />
+        <Info
+          label={<FormattedMessage id="cardBin" />}
+          value={`${cardBin} **** `}
+        />
+        <Info label={<FormattedMessage id="reference" />} value={reference} />
+        <Info label={<FormattedMessage id="errorCode" />} value={errorCode} />
+      </Section>
+
+      <Section title={<FormattedMessage id="client_data" />}>
+        <Info label={<FormattedMessage id="fullName" />} value={fullName} />
+        <Info label={<FormattedMessage id="email" />} value={email} />
+        <Info label={<FormattedMessage id="address" />} value={address} />
+        <Info label={<FormattedMessage id="country" />} value={country} />
+      </Section>
+
+      <Section title={<FormattedMessage id="payment_method" />}>
+        <Info
+          label={<FormattedMessage id="cardPan" />}
+          value={`**** ${cardPan}`}
+        />
+        {cardExpirationDate && (
+          <Info
+            label={<FormattedMessage id="cardExpirationDate" />}
+            value={format(cardExpirationDate, "dd/MM/yy")}
+          />
+        )}
+        <Info
+          label={<FormattedMessage id="cardCountry" />}
+          value={cardCountry}
+        />
+        <Info label={<FormattedMessage id="clientIp" />} value={clientIp} />
+      </Section>
+
+      <Section title={<FormattedMessage id="schedule" />}>
+        {createdAt && (
+          <Info
+            label={<FormattedMessage id="createdAt" />}
+            value={createdAt && format(new Date(createdAt), "dd/MM/yy")}
+          />
+        )}
+        {completedAt && (
+          <Info
+            label={<FormattedMessage id="completedAt" />}
+            value={completedAt && format(new Date(completedAt), "dd/MM/yy")}
+          />
+        )}
+      </Section>
+
+      <div className="flex mt-4 gap-4">
+        <Button
+          variant="outline"
+          className="w-full md:w-1/2"
+          onClick={onDownload}
+        >
+          <Download />
+          <FormattedMessage id="download" />
+        </Button>
+        <Button variant="default" className="w-full md:w-1/2">
+          <FormattedMessage id="refound" />
+        </Button>
+      </div>
+    </div>
   )
 }
--- src/components/DataFull.tsx
+++ src/components/DataFull.tsx
@@ -26,8 +26,8 @@ export default async function DataFull({
     <>
       {dehydratedState && (
         <DataComponent
+          key={key}
           {...{
-            key,
             queryConfig,
             dehydratedState,
             searchParams: urlParams,
