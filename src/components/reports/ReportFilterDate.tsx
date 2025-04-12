485ee6d07e041069fb50903b8c6f36ec931fb126
Arliz Heredia Rodríguez <arliz.heredia@tropipay.com>
2025-04-12 10:16:32 -0400

    fix: Settings for the generated PDF report

--- src/components/reports/ReportFilterDate.tsx
+++ src/components/reports/ReportFilterDate.tsx
@@ -3,18 +3,13 @@
 import { Button } from "@/components/ui/button"
 import { Calendar } from "@/components/ui/calendar"
 import {
-  Command,
-  CommandGroup,
-  CommandItem,
-  CommandList,
-} from "@/components/ui/command"
-import {
   Popover,
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover"
 import { cn, getDatePeriods } from "@/lib/utils"
 import { PopoverClose } from "@radix-ui/react-popover"
+import { SelectItem } from "@radix-ui/react-select"
 import { format, isAfter, isBefore, parse, startOfDay } from "date-fns"
 import { CalendarDays, CalendarIcon, CheckIcon } from "lucide-react"
 import React, { useState } from "react"
@@ -32,7 +27,17 @@ export function ReportFilterDate({ onChange }: Props) {
   const defaultMonth = months[0]
 
   const { t } = useTranslation()
-  const [range, setRange] = useState(defaultMonth.label)
+  const [currentRange, setCurrentRange] = useState<string | undefined>(
+    defaultMonth.label
+  )
+  const [currentFromDate, setCurrentFromDate] = React.useState<
+    string | undefined
+  >(format(defaultMonth.from, "dd/MM/yyyy"))
+  const [currentToDate, setCurrentToDate] = React.useState<string | undefined>(
+    format(defaultMonth.to, "dd/MM/yyyy")
+  )
+
+  const [range, setRange] = useState<string | undefined>(defaultMonth.label)
   const [fromDate, setFromDate] = React.useState<string | undefined>(
     format(defaultMonth.from, "dd/MM/yyyy")
   )
@@ -41,11 +46,32 @@ export function ReportFilterDate({ onChange }: Props) {
   )
   const [error, setError] = React.useState<string | null>(null)
   const today = React.useMemo(() => startOfDay(new Date()), [])
+  const startDate = React.useMemo(() => startOfDay(new Date(2025, 0, 1)), [])
+
+  const isApplyButtonDisabled = React.useMemo(() => {
+    if (!fromDate && !toDate) return true
+    const fromParsed = fromDate ? parse(fromDate, "dd/MM/yyyy", new Date()) : ""
+    const toParsed = toDate ? parse(toDate, "dd/MM/yyyy", new Date()) : ""
+    if (fromParsed && toDate && isAfter(fromParsed, toParsed)) return true
+    return false
+  }, [fromDate, toDate])
+
+  const handlePeriodChange = (value: string) => {
+    const month = months.find(({ label }) => label === value)
+
+    if (!!month) {
+      setRange(value)
+      setFromDate(format(month.from, "dd/MM/yyyy"))
+      setToDate(format(month.to, "dd/MM/yyyy"))
+      setError(null)
+    }
+  }
 
   const handleDateChange = React.useCallback(
     (key: "from" | "to", selectedDate: Date | undefined) => {
       if (!selectedDate) {
         key === "from" ? setFromDate(undefined) : setToDate(undefined)
+        setRange(undefined)
         setError(null)
         return
       }
@@ -70,9 +96,9 @@ export function ReportFilterDate({ onChange }: Props) {
 
       setError(null)
       key === "from" ? setFromDate(formattedDate) : setToDate(formattedDate)
-      setRange("")
+      setRange(undefined)
     },
-    [fromDate, toDate, t]
+    [fromDate, toDate]
   )
 
   const handleApplyFilter = React.useCallback(() => {
@@ -86,25 +112,45 @@ export function ReportFilterDate({ onChange }: Props) {
     setError(null)
 
     if (fromDate || toDate) {
+      setCurrentRange(range)
+      setCurrentFromDate(fromDate)
+      setCurrentToDate(toDate)
       onChange?.([fromDate, toDate].join(","))
     }
-  }, [fromDate, toDate, t])
+  }, [fromDate, toDate])
+
+  const disableBefore1rst2025 = React.useCallback(
+    (date: Date) => isBefore(date, startDate),
+    [today]
+  )
+  const disableBeforeFromDate = React.useCallback(
+    (date: Date) =>
+      fromDate
+        ? isBefore(date, parse(fromDate, "dd/MM/yyyy", new Date()))
+        : false,
+    [fromDate]
+  )
+  const disableAfterToDate = React.useCallback(
+    (date: Date) =>
+      toDate ? isAfter(date, parse(toDate, "dd/MM/yyyy", new Date())) : false,
+    [toDate]
+  )
 
   return (
     <Popover>
       <PopoverTrigger asChild>
         <Button variant="outline">
           <CalendarDays />
-          {range ? (
-            <span>{range}</span>
+          {currentRange ? (
+            <span>{currentRange}</span>
           ) : (
             <>
-              {fromDate && toDate
-                ? `${fromDate} - ${toDate}`
-                : fromDate
-                ? `${t("from")} ${fromDate}`
-                : toDate
-                ? `${t("to")} ${toDate}`
+              {currentFromDate && currentToDate
+                ? `${currentFromDate} - ${currentToDate}`
+                : currentFromDate
+                ? `${t("from")} ${currentFromDate}`
+                : currentToDate
+                ? `${t("to")} ${currentToDate}`
                 : null}
             </>
           )}
@@ -121,42 +167,26 @@ export function ReportFilterDate({ onChange }: Props) {
             <FormattedMessage id="date" />
           </h2>
           <div className="mb-2">
-            <Label htmlFor="date-to" className="my-2">
+            <Label className="my-2">
               <FormattedMessage id="period" />
             </Label>
-            <Select>
+            <Select value={range} onValueChange={handlePeriodChange}>
               <SelectTrigger aria-label={t("select_period")}>
-                <SelectValue placeholder={t("select_period")} />
+                <SelectValue>
+                  {!!range ? range : t("select_period")}
+                </SelectValue>
               </SelectTrigger>
               <SelectContent position="popper">
-                <Command>
-                  <CommandList>
-                    <CommandGroup heading="" className="p-3">
-                      {months.map(({ label, from, to }) => (
-                        <CommandItem
-                          key={label}
-                          onSelect={() => {
-                            setRange(label)
-                            setFromDate(format(from, "dd/MM/yyyy"))
-                            setToDate(format(to, "dd/MM/yyyy"))
-                            onChange?.(
-                              [
-                                format(from, "dd/MM/yyyy"),
-                                format(to, "dd/MM/yyyy"),
-                              ].join(",")
-                            )
-                          }}
-                          className="cursor-pointer flex items-center justify-between"
-                        >
-                          {label}
-                          {range === label && (
-                            <CheckIcon className="mr-2 h-4 w-4" />
-                          )}
-                        </CommandItem>
-                      ))}
-                    </CommandGroup>
-                  </CommandList>
-                </Command>
+                {months.map(({ label }) => (
+                  <SelectItem
+                    key={label}
+                    value={label}
+                    className="cursor-pointer flex items-center justify-between p-2"
+                  >
+                    {label}
+                    {range === label && <CheckIcon className="mr-2 h-4 w-4" />}
+                  </SelectItem>
+                ))}
               </SelectContent>
             </Select>
           </div>
@@ -208,6 +238,9 @@ export function ReportFilterDate({ onChange }: Props) {
                     : undefined
                 }
                 onSelect={(date) => handleDateChange("from", date)}
+                disabled={(date) =>
+                  disableBefore1rst2025(date) || disableAfterToDate(date)
+                }
               />
             </PopoverContent>
           </Popover>
@@ -257,13 +290,21 @@ export function ReportFilterDate({ onChange }: Props) {
                   toDate ? parse(toDate, "dd/MM/yyyy", new Date()) : undefined
                 }
                 onSelect={(date) => handleDateChange("to", date)}
+                disabled={(date) =>
+                  disableBefore1rst2025(date) || disableBeforeFromDate(date)
+                }
               />
             </PopoverContent>
           </Popover>
           {error && <div className="text-red-500 text-sm mb-2">{error}</div>}
           <div className="flex gap-2 mt-2">
             <PopoverClose asChild>
-              <Button variant="default" className="w-full" type="submit">
+              <Button
+                variant="default"
+                className="w-full"
+                type="submit"
+                disabled={isApplyButtonDisabled}
+              >
                 <FormattedMessage id="apply" />
               </Button>
             </PopoverClose>
