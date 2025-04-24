1937a5624b81956d0887effb3449726451467206
Arliz Heredia Rodríguez <arliz.heredia@tropipay.com>
2025-04-24 18:44:50 -0400

    refactor: refactor drawer components

--- src/app/dashboard/movements/movementDetail.tsx
+++ src/app/dashboard/movements/movementDetail.tsx
@@ -71,121 +71,128 @@ export default function MovementDetail(props: any): JSX.Element {
 
   return (
     <div className="max-w-md mx-auto p-4">
-      <div className="flex justify-between items-center mb-3">
-        <div className="font-poppins text-2xl leading-5 tracking-tight uppercase font-bold">
-          {amount.value > 0 ? "+" : ""}
-          {formatAmount(amount.value, amount.currency, "right")}
+      <div>
+        <div className="flex justify-between items-center mb-3">
+          <div className="font-poppins text-2xl leading-5 tracking-tight uppercase font-bold">
+            {amount.value > 0 ? "+" : ""}
+            {formatAmount(amount.value, amount.currency, "right")}
+          </div>
+          <FacetedBadge
+            value={state}
+            optionList={movementStates}
+            optionListGroups={movementStateGroups}
+          />
         </div>
-        <FacetedBadge
-          value={state}
-          optionList={movementStates}
-          optionListGroups={movementStateGroups}
-        />
+        <div className="flex justify-between items-center mb-4 pb-1">
+          <p className="text-xs text-gray-500"> {bankOrderCode}</p>
+          {completedAt && (
+            <p className="text-xs text-gray-500">
+              {format(new Date(completedAt), "dd/MM/yy HH:mm")}
+            </p>
+          )}
+        </div>
+        <Section title={<FormattedMessage id="movement_data" />}>
+          <Info
+            label={<FormattedMessage id="type" />}
+            value={<FormattedMessage id={`mt_${type}`} />}
+          />
+          <Info label={<FormattedMessage id="product" />} value={product} />
+          <Info label={<FormattedMessage id="concept" />} value={concept} />
+        </Section>
+
+        <Section title={<FormattedMessage id="beneficiary_data" />}>
+          <Info label={<FormattedMessage id="alias" />} value={alias} />
+          <Info
+            label={<FormattedMessage id="name" />}
+            value={<span className="uppercase">{name}</span>}
+          />
+          <Info label={<FormattedMessage id="account" />} value={account} />
+          <Info label={<FormattedMessage id="email" />} value={email} />
+          <Info label={<FormattedMessage id="country" />} value={country} />
+        </Section>
+
+        <Section title={<FormattedMessage id="sender_data" />}>
+          <Info
+            label={<FormattedMessage id="name" />}
+            value={<span className="uppercase">{senderName}</span>}
+          />
+          <Info label={<FormattedMessage id="email" />} value={senderEmail} />
+          <Info
+            label={<FormattedMessage id="address" />}
+            value={clientAddress}
+          />
+          <Info
+            label={<FormattedMessage id="country" />}
+            value={senderCountry}
+          />
+        </Section>
+
+        <Section title={<FormattedMessage id="imports" />}>
+          <Info
+            label={<FormattedMessage id="import" />}
+            value={formatAmount(amount.value, amount.currency, "right")}
+          />
+          {conversionRate && fee && amount.currency !== fee.currency && (
+            <Info
+              label={<FormattedMessage id="conversionRate" />}
+              value={`1 EUR = ${conversionRate} ${amount.currency}`}
+            />
+          )}
+          {fee && (
+            <Info
+              label={<FormattedMessage id="fee" />}
+              value={formatAmount(fee.value, fee.currency, "right")}
+            />
+          )}
+          <Info
+            label={<FormattedMessage id="netAmount" />}
+            value={`${formatAmount(
+              netAmount.value,
+              netAmount.currency,
+              "right"
+            )}`}
+          />
+        </Section>
+
+        <Section title={<FormattedMessage id="payment_method" />}>
+          <Info label={<FormattedMessage id="cardType" />} value={cardType} />
+          <Info
+            label={<FormattedMessage id="account" />}
+            value="******** FALTA CUENTA"
+          />
+          <Info
+            label={<FormattedMessage id="cardPan" />}
+            value={`**** ${cardPan}`}
+          />
+          {cardExpirationDate && (
+            <Info
+              label={<FormattedMessage id="cardExpirationDate" />}
+              value={format(cardExpirationDate, "dd/MM/yy")}
+            />
+          )}
+          <Info
+            label={<FormattedMessage id="cardCountry" />}
+            value={cardCountry}
+          />
+          <Info label={<FormattedMessage id="clientIp" />} value={clientIp} />
+        </Section>
+
+        <Section title={<FormattedMessage id="schedule" />}>
+          {createdAt && (
+            <Info
+              label={<FormattedMessage id="createdAt" />}
+              value={format(new Date(createdAt), "dd/MM/yy")}
+            />
+          )}
+          {completedAt && (
+            <Info
+              label={<FormattedMessage id="completedAt" />}
+              value={format(new Date(completedAt), "dd/MM/yy")}
+            />
+          )}
+        </Section>
       </div>
-      <div className="flex justify-between items-center mb-4 pb-1">
-        <p className="text-xs text-gray-500"> {bankOrderCode}</p>
-        {completedAt && (
-          <p className="text-xs text-gray-500">
-            {format(new Date(completedAt), "dd/MM/yy HH:mm")}
-          </p>
-        )}
-      </div>
-      <Section title={<FormattedMessage id="movement_data" />}>
-        <Info
-          label={<FormattedMessage id="type" />}
-          value={<FormattedMessage id={`mt_${type}`} />}
-        />
-        <Info label={<FormattedMessage id="product" />} value={product} />
-        <Info label={<FormattedMessage id="concept" />} value={concept} />
-      </Section>
-
-      <Section title={<FormattedMessage id="beneficiary_data" />}>
-        <Info label={<FormattedMessage id="alias" />} value={alias} />
-        <Info
-          label={<FormattedMessage id="name" />}
-          value={<span className="uppercase">{name}</span>}
-        />
-        <Info label={<FormattedMessage id="account" />} value={account} />
-        <Info label={<FormattedMessage id="email" />} value={email} />
-        <Info label={<FormattedMessage id="country" />} value={country} />
-      </Section>
-
-      <Section title={<FormattedMessage id="sender_data" />}>
-        <Info
-          label={<FormattedMessage id="name" />}
-          value={<span className="uppercase">{senderName}</span>}
-        />
-        <Info label={<FormattedMessage id="email" />} value={senderEmail} />
-        <Info label={<FormattedMessage id="address" />} value={clientAddress} />
-        <Info label={<FormattedMessage id="country" />} value={senderCountry} />
-      </Section>
-
-      <Section title={<FormattedMessage id="imports" />}>
-        <Info
-          label={<FormattedMessage id="import" />}
-          value={formatAmount(amount.value, amount.currency, "right")}
-        />
-        {conversionRate && fee && amount.currency !== fee.currency && (
-          <Info
-            label={<FormattedMessage id="conversionRate" />}
-            value={`1 EUR = ${conversionRate} ${amount.currency}`}
-          />
-        )}
-        {fee && (
-          <Info
-            label={<FormattedMessage id="fee" />}
-            value={formatAmount(fee.value, fee.currency, "right")}
-          />
-        )}
-        <Info
-          label={<FormattedMessage id="netAmount" />}
-          value={`${formatAmount(
-            netAmount.value,
-            netAmount.currency,
-            "right"
-          )}`}
-        />
-      </Section>
-
-      <Section title={<FormattedMessage id="payment_method" />}>
-        <Info label={<FormattedMessage id="cardType" />} value={cardType} />
-        <Info
-          label={<FormattedMessage id="account" />}
-          value="******** FALTA CUENTA"
-        />
-        <Info
-          label={<FormattedMessage id="cardPan" />}
-          value={`**** ${cardPan}`}
-        />
-        {cardExpirationDate && (
-          <Info
-            label={<FormattedMessage id="cardExpirationDate" />}
-            value={format(cardExpirationDate, "dd/MM/yy")}
-          />
-        )}
-        <Info
-          label={<FormattedMessage id="cardCountry" />}
-          value={cardCountry}
-        />
-        <Info label={<FormattedMessage id="clientIp" />} value={clientIp} />
-      </Section>
-
-      <Section title={<FormattedMessage id="schedule" />}>
-        {createdAt && (
-          <Info
-            label={<FormattedMessage id="createdAt" />}
-            value={format(new Date(createdAt), "dd/MM/yy")}
-          />
-        )}
-        {completedAt && (
-          <Info
-            label={<FormattedMessage id="completedAt" />}
-            value={format(new Date(completedAt), "dd/MM/yy")}
-          />
-        )}
-      </Section>
-
-      <div className="flex mt-4 gap-4">
+      <div className="flex mt-4 gap-4 w-full p-4 bg-white absolute bottom-0 left-0">
         <Button
           variant="outline"
           className="w-full"
