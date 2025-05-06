06d67f8798e2d1b3deaf60da9643d61030f01630
serguitus <serguitus@gmail.com>
2025-05-06 16:35:53 -0600

    adding env variables to local env

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-  output: 'standalone'
+  output: 'standalone',
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL
+  }
 }
 
 export default nextConfig
