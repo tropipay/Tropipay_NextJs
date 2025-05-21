f503f757843df7ace18a9f7c96dff287ba11cd4b
Luis Saenz <luis.saenz@tropipay.com>
2025-05-21 18:54:31 -0300

    Revert "update DataFull"
    This reverts commit 977d282e03e5d225fe28eb907c7e801debf1845e.

--- src/components/DataFull.tsx
+++ src/components/DataFull.tsx
@@ -1,5 +1,4 @@
 import DataComponent from "@/components/DataComponent"
-import ErrorMessage from "@/components/ErrorMessage"
 import { fetchData } from "@/utils/data/fetchData"
 import { processQueryParameters } from "@/utils/data/utils"
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query"
@@ -17,42 +16,27 @@ export default async function DataFull({
   searchParams = {},
   mockData,
 }: DataFullProps) {
-  try {
-    const queryClient = getQueryClient()
-    const urlParams = await processQueryParameters(searchParams)
-    await fetchData(queryClient, queryConfig, urlParams)
-    const { key } = queryConfig
-    const dehydratedState = dehydrate(queryClient)
-
-    return (
-      <>
-        <HydrationBoundary state={dehydratedState}>
-          {dehydratedState ? (
-            <DataComponent
-              key={key}
-              showLoading
-              showError
-              {...{
-                queryConfig,
-                searchParams: urlParams,
-                mockData,
-              }}
-            >
-              <>{children}</>
-            </DataComponent>
-          ) : (
-            <div> Loading... </div>
-          )}
-        </HydrationBoundary>
-      </>
-    )
-  } catch (error) {
-    return (
-      <div className="w-full max-w-[500px] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-        <ErrorMessage>
-          {error instanceof Error ? error.message : "Failed to load data"}
-        </ErrorMessage>
-      </div>
-    )
-  }
+  const queryClient = getQueryClient()
+  const urlParams = await processQueryParameters(searchParams)
+  await fetchData(queryClient, queryConfig, urlParams)
+  const { key } = queryConfig
+  const dehydratedState = dehydrate(queryClient)
+  // console.log("dehydratedState:", dehydratedState)
+  return (
+    <>
+      <HydrationBoundary state={dehydratedState}>
+        <DataComponent
+          key={key}
+          showLoading
+          {...{
+            queryConfig,
+            searchParams: urlParams,
+            mockData,
+          }}
+        >
+          <>{children}</>
+        </DataComponent>
+      </HydrationBoundary>
+    </>
+  )
 }
