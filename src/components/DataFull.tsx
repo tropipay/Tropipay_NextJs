6f8a000a8db5b69184e81d449d86b67a1d38ca11
Luis Saenz <luis.saenz@tropipay.com>
2025-05-21 19:27:17 -0300

    update DataFull

--- src/components/DataFull.tsx
+++ src/components/DataFull.tsx
@@ -3,9 +3,11 @@ import { fetchData } from "@/utils/data/fetchData"
 import { processQueryParameters } from "@/utils/data/utils"
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query"
 import getQueryClient from "./GetQueryClient"
+import { ReactElement } from "react"
+
 interface DataFullProps {
   queryConfig: any
-  children: React.ReactNode
+  children: ReactElement
   searchParams?: { [key: string]: string }
   mockData?: any
 }
@@ -21,22 +23,20 @@ export default async function DataFull({
   await fetchData(queryClient, queryConfig, urlParams)
   const { key } = queryConfig
   const dehydratedState = dehydrate(queryClient)
-  // console.log("dehydratedState:", dehydratedState)
+
   return (
-    <>
-      <HydrationBoundary state={dehydratedState}>
-        <DataComponent
-          key={key}
-          showLoading
-          {...{
-            queryConfig,
-            searchParams: urlParams,
-            mockData,
-          }}
-        >
-          <>{children}</>
-        </DataComponent>
-      </HydrationBoundary>
-    </>
+    <HydrationBoundary state={dehydratedState}>
+      <DataComponent
+        key={key}
+        showLoading
+        {...{
+          queryConfig,
+          searchParams: urlParams,
+          mockData,
+        }}
+      >
+        {children}
+      </DataComponent>
+    </HydrationBoundary>
   )
 }
